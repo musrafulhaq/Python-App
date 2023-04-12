533c54fd2ddf5854a73e4aaeed83d7d33e21f4e1
musrafulhaq <musrafulhaque.kat@gmail.com>
2023-04-12 13:27:33 +0530

    commit to wi 3

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, I'm testing in ewm....for integrating EWM to Github..."
+    return "Hello, I'm testing in ewm"
 
 if __name__ == "__main__":
     app.run(debug=True)
